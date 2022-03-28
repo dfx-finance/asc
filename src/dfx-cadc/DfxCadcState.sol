4a6d402e330fb4d3caae510716d770068633002f
adrianmcli <li.adrianmc@gmail.com>
2022-03-28 07:13:05 -0700

    make state point to new ERC20Upgradeable.sol

--- src/dfx-cadc/DfxCadcState.sol
+++ src/dfx-cadc/DfxCadcState.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.10;
 import "@openzeppelin-upgradeable/contracts/security/ReentrancyGuardUpgradeable.sol";
 import "@openzeppelin-upgradeable/contracts/security/PausableUpgradeable.sol";
 import "@openzeppelin-upgradeable/contracts/access/AccessControlUpgradeable.sol";
-import "@openzeppelin-upgradeable/contracts/token/ERC20/ERC20Upgradeable.sol";
+import "./ERC20Upgradeable.sol";
 
 contract DfxCadcState is
     AccessControlUpgradeable,
