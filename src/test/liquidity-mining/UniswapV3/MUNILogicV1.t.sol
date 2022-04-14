70314322380a53ac24d8df13d2c655e576e8e215
K Fitzsimmons <kafitz22@gmail.com>
2022-04-14 15:50:41 -0400

    Added additional tests

--- src/test/liquidity-mining/UniswapV3/MUNILogicV1.t.sol
+++ src/test/liquidity-mining/UniswapV3/MUNILogicV1.t.sol
@@ -130,6 +130,47 @@ contract UpgradeableMUNITest is DSTest, stdCheats {
         proxiedMuniLogic = MUNILogicV1(address(upgradeableProxy));        
     }
 
+    function test_pool_nftpositionmanager() public {
+        uint160 lowerSqrtPriceX96 = FixedPoint
+            .fraction(1, 2)
+            .sqrt()
+            .mul(2**96)
+            .decode144();
+        uint160 upperSqrtPriceX96 = FixedPoint
+            .fraction(2, 1)
+            .sqrt()
+            .mul(2**96)
+            .decode144();
+
+        int24 lowerTick = TickMath.getTickAtSqrtRatio(lowerSqrtPriceX96);
+        lowerTick = lowerTick - (lowerTick % tickSpacing);
+
+        int24 upperTick = TickMath.getTickAtSqrtRatio(upperSqrtPriceX96);
+        upperTick = upperTick - (upperTick % tickSpacing) + tickSpacing;
+
+        token0.mint(address(this), 1_000_000_000e18);
+        token1.mint(address(this), 1_000_000_000e18);
+
+        token0.approve(address(univ3PosManager), 1_000_000_000e18);
+        token1.approve(address(univ3PosManager), 1_000_000_000e18);
+
+        univ3PosManager.mint(
+            INonfungiblePositionManager.MintParams({
+                token0: pool.token0(),
+                token1: pool.token1(),
+                fee: fee,
+                tickLower: lowerTick,
+                tickUpper: upperTick,
+                amount0Desired: 1_000_000_000e18,
+                amount1Desired: 1_000_000_000e18,
+                amount0Min: 0e18,
+                amount1Min: 0e18,
+                recipient: address(this),
+                deadline: block.timestamp + 1
+            })
+        );
+    }
+
     function test_muniproxy_mint(uint128 tokenAmount)
         public
         returns (
@@ -168,5 +209,69 @@ contract UpgradeableMUNITest is DSTest, stdCheats {
 
         assertTrue(delta0 > 98e16 && delta0 <= 1e18);
         assertTrue(delta1 > 98e16 && delta1 <= 1e18);
+    }
+
+    function test_muniproxy_burn(uint128 tokenAmount) public {
+        test_muniproxy_mint(tokenAmount);
+
+        assertEq(token0.balanceOf(address(this)), 0);
+        assertEq(token1.balanceOf(address(this)), 0);
+
+        uint256 lpAmount = proxiedMuniLogic.balanceOf(address(this));
+        (uint256 amount0, uint256 amount1) = proxiedMuniLogic.getBurnAmounts(lpAmount);
+
+        assertGt(lpAmount, 0);
+        assertGt(amount0, 0);
+        assertGt(amount1, 1);
+
+        proxiedMuniLogic.burn(lpAmount, address(this));
+        assertEq(token0.balanceOf(address(this)), amount0);
+        assertEq(token1.balanceOf(address(this)), amount1);
+    }
+
+    function test_muniproxy_rebalance(uint128 tokenAmount) public {
+        test_pool_nftpositionmanager();
+        (uint256 aAmount0, uint256 aAmount1, uint256 aLpAmount) = test_muniproxy_mint(tokenAmount);
+
+        int24 lowerTick = proxiedMuniLogic.lowerTick();
+        lowerTick = lowerTick - tickSpacing;
+
+        int24 upperTick = proxiedMuniLogic.upperTick();
+        upperTick = upperTick + tickSpacing;
+
+        uint160 swapThresholdPriceX96 = FixedPoint
+            .fraction(1, 2)
+            .sqrt()
+            .mul(2**96)
+            .decode144();
+
+        // Should have less liquidity in ticks
+        uint256 liquidityBefore = pool.liquidity();
+        proxiedMuniLogic.executiveRebalance(
+            lowerTick,
+            upperTick,
+            swapThresholdPriceX96,
+            0,
+            true
+        );
+        uint256 liquidityAfter = pool.liquidity();
+
+        assertGt(liquidityBefore, 0);
+        assertGt(liquidityAfter, 0);
+        assertLt(liquidityAfter, liquidityBefore);
+
+        // Make sure we can still remove roughly the same tokenAmount
+        uint256 lpAmount = proxiedMuniLogic.balanceOf(address(this));
+        (uint256 amount0, uint256 amount1) = proxiedMuniLogic.getBurnAmounts(lpAmount);
+
+        // LP amount shouldn't change
+        assertEq(lpAmount, aLpAmount);
+
+        // amount0 and amount1 should be roughly similar
+        uint256 delta0 = amount0 * 1e18 / aAmount0;
+        uint256 delta1 = amount1 * 1e18 / aAmount1;
+
+        assertTrue(delta0 > 99e16 && delta0 <= 1e18);
+        assertTrue(delta1 > 99e16 && delta1 <= 1e18);
     }
 }
