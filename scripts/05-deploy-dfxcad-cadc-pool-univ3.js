149b789e788df3b7a2ea48baebdd2ba49d89a1fd
HL <36248745+honestbonsai@users.noreply.github.com>
2022-06-16 15:27:40 -0400

    Apply auto formatting

--- scripts/05-deploy-dfxcad-cadc-pool-univ3.js
+++ scripts/05-deploy-dfxcad-cadc-pool-univ3.js
@@ -1,18 +1,18 @@
-import fs from 'fs'
-import path from 'path'
-import ethers from 'ethers'
-import { fileURLToPath } from 'url';
-import { deployContract, wallet } from './common.js'
+import fs from "fs";
+import path from "path";
+import ethers from "ethers";
+import { fileURLToPath } from "url";
+import { deployContract, wallet } from "./common.js";
 
 import FiatTokenV2Artifact from "./abis/FiatTokenV2.json";
-import IUniswapV3FactoryArtifact from '@uniswap/v3-core/artifacts/contracts/interfaces/IUniswapV3Factory.sol/IUniswapV3Factory.json';
-import IUniswapV3PoolArtifact from '@uniswap/v3-core/artifacts/contracts/interfaces/IUniswapV3Pool.sol/IUniswapV3Pool.json';
+import IUniswapV3FactoryArtifact from "@uniswap/v3-core/artifacts/contracts/interfaces/IUniswapV3Factory.sol/IUniswapV3Factory.json";
+import IUniswapV3PoolArtifact from "@uniswap/v3-core/artifacts/contracts/interfaces/IUniswapV3Pool.sol/IUniswapV3Pool.json";
 
-import DfxCadLogicV1Artifact from '../out/DfxCadLogicV1.sol/DfxCadLogicV1.json';
-import MUNILogicV1Artifact from '../out/MUNILogicV1.sol/MUNILogicV1.json';
-import ASCUpgradableProxyArtifact from '../out/ASCUpgradableProxy.sol/ASCUpgradableProxy.json';
+import DfxCadLogicV1Artifact from "../out/DfxCadLogicV1.sol/DfxCadLogicV1.json";
+import MUNILogicV1Artifact from "../out/MUNILogicV1.sol/MUNILogicV1.json";
+import ASCUpgradableProxyArtifact from "../out/ASCUpgradableProxy.sol/ASCUpgradableProxy.json";
 
-import { encodeSqrtRatioX96, FeeAmount, TickMath } from '@uniswap/v3-sdk';
+import { encodeSqrtRatioX96, FeeAmount, TickMath } from "@uniswap/v3-sdk";
 
 const { formatUnits } = ethers.utils;
 
@@ -26,111 +26,132 @@ const UNISWAP_FACTORY = "0x1F98431c8aD98523631AE4a59f267346ea31F984";
 // Access controller
 const DFX_GOV_MULTISIG = "0x27E843260c71443b4CC8cB6bF226C3f77b9695AF";
 
-
 const main = async () => {
-    const dfxCadToken = new ethers.Contract(DFXCAD, DfxCadLogicV1Artifact.abi, wallet);
-    const cadcToken = new ethers.Contract(CADC, FiatTokenV2Artifact, wallet);
-    const uniswapV3Factory = new ethers.Contract(UNISWAP_FACTORY, IUniswapV3FactoryArtifact.abi, wallet);
-    const MUNILogicFactory = new ethers.ContractFactory(
-        MUNILogicV1Artifact.abi, MUNILogicV1Artifact.bytecode.object, wallet
-    );
-    const UpgradableProxyFactory = new ethers.ContractFactory(
-        ASCUpgradableProxyArtifact.abi, ASCUpgradableProxyArtifact.bytecode.object, wallet
-    );   
+  const dfxCadToken = new ethers.Contract(
+    DFXCAD,
+    DfxCadLogicV1Artifact.abi,
+    wallet
+  );
+  const cadcToken = new ethers.Contract(CADC, FiatTokenV2Artifact, wallet);
+  const uniswapV3Factory = new ethers.Contract(
+    UNISWAP_FACTORY,
+    IUniswapV3FactoryArtifact.abi,
+    wallet
+  );
+  const MUNILogicFactory = new ethers.ContractFactory(
+    MUNILogicV1Artifact.abi,
+    MUNILogicV1Artifact.bytecode.object,
+    wallet
+  );
+  const UpgradableProxyFactory = new ethers.ContractFactory(
+    ASCUpgradableProxyArtifact.abi,
+    ASCUpgradableProxyArtifact.bytecode.object,
+    wallet
+  );
 
-    // Check token addresses both exist
-    const dfxCadBalance = await dfxCadToken.balanceOf(wallet.address);
-    const cadcBalance = await cadcToken.balanceOf(wallet.address);
-    console.log(`Total balance--dfxCad: ${formatUnits(dfxCadBalance)} cadc: ${formatUnits(cadcBalance)}`);
+  // Check token addresses both exist
+  const dfxCadBalance = await dfxCadToken.balanceOf(wallet.address);
+  const cadcBalance = await cadcToken.balanceOf(wallet.address);
+  console.log(
+    `Total balance--dfxCad: ${formatUnits(dfxCadBalance)} cadc: ${formatUnits(
+      cadcBalance
+    )}`
+  );
 
-    // Create the new Uniswap pool
-    console.log("Creating UniswapV3 dfxCAD/CADC pool...");
-    const feeAmount = FeeAmount.LOWEST;
-    await uniswapV3Factory.createPool(dfxCadToken.address, cadcToken.address, feeAmount);
+  // Create the new Uniswap pool
+  console.log("Creating UniswapV3 dfxCAD/CADC pool...");
+  const feeAmount = FeeAmount.LOWEST;
+  await uniswapV3Factory.createPool(
+    dfxCadToken.address,
+    cadcToken.address,
+    feeAmount
+  );
 
-    // Fetch pool's address
-    const poolAddress = await uniswapV3Factory.getPool(dfxCadToken.address, cadcToken.address, feeAmount);
-    console.log("Pool address:", poolAddress)
+  // Fetch pool's address
+  const poolAddress = await uniswapV3Factory.getPool(
+    dfxCadToken.address,
+    cadcToken.address,
+    feeAmount
+  );
+  console.log("Pool address:", poolAddress);
 
-    // Instantiate ethers ABI on pool address
-    console.log("Initializing UniswapV3 dfxCAD/CADC pool at 1 dfxCAD:1CADC...")
-    const DfxCadCadcPool = new ethers.Contract(
-        poolAddress, IUniswapV3PoolArtifact.abi, wallet,
-    );
+  // Instantiate ethers ABI on pool address
+  console.log("Initializing UniswapV3 dfxCAD/CADC pool at 1 dfxCAD:1CADC...");
+  const DfxCadCadcPool = new ethers.Contract(
+    poolAddress,
+    IUniswapV3PoolArtifact.abi,
+    wallet
+  );
 
-    // Create sqrtPrice from 1 dfxCAD:1 CADC and initialize pool
-    const sqrtRatioX96 = encodeSqrtRatioX96(1, 1);
-    await DfxCadCadcPool.connect(wallet).initialize(sqrtRatioX96.toString());
+  // Create sqrtPrice from 1 dfxCAD:1 CADC and initialize pool
+  const sqrtRatioX96 = encodeSqrtRatioX96(1, 1);
+  await DfxCadCadcPool.connect(wallet).initialize(sqrtRatioX96.toString());
 
-    // Increase TWAP window like in Muni.t.sol tests
-    await DfxCadCadcPool.increaseObservationCardinalityNext(5);
+  // Increase TWAP window like in Muni.t.sol tests
+  await DfxCadCadcPool.increaseObservationCardinalityNext(5);
 
-    const tickSpacing = await DfxCadCadcPool.tickSpacing();
-    const lowerSqrtPriceX96 = encodeSqrtRatioX96(980, 1000);
-    const upperSqrtPriceX96 = encodeSqrtRatioX96(1020, 1000);
-    const lowerTickAmount = TickMath.getTickAtSqrtRatio(lowerSqrtPriceX96);
-    const upperTickAmount = TickMath.getTickAtSqrtRatio(upperSqrtPriceX96);
-    const lowerTick = lowerTickAmount - (lowerTickAmount % tickSpacing);
-    const upperTick = upperTickAmount - (upperTickAmount % tickSpacing);
+  const tickSpacing = await DfxCadCadcPool.tickSpacing();
+  const lowerSqrtPriceX96 = encodeSqrtRatioX96(980, 1000);
+  const upperSqrtPriceX96 = encodeSqrtRatioX96(1020, 1000);
+  const lowerTickAmount = TickMath.getTickAtSqrtRatio(lowerSqrtPriceX96);
+  const upperTickAmount = TickMath.getTickAtSqrtRatio(upperSqrtPriceX96);
+  const lowerTick = lowerTickAmount - (lowerTickAmount % tickSpacing);
+  const upperTick = upperTickAmount - (upperTickAmount % tickSpacing);
 
-    // Deploy MUNI contract
-    const muniLogicV1 = await deployContract({
-        name: 'MUNILogicV1',
-        deployer: wallet,
-        factory: MUNILogicFactory,
-        args: [],
-        opts: {
-            // maxFeePerGas: 54796832180,
-        }
-    });
+  // Deploy MUNI contract
+  const muniLogicV1 = await deployContract({
+    name: "MUNILogicV1",
+    deployer: wallet,
+    factory: MUNILogicFactory,
+    args: [],
+    opts: {
+      // maxFeePerGas: 54796832180,
+    },
+  });
 
-    // Deploy ASCUpgradableProxy with the encoded args and initialize MUNI pool
-    const calldata = MUNILogicFactory.interface.encodeFunctionData("initialize", [
-        DFX_GOV_MULTISIG,  // owner
-        poolAddress,
-        FeeAmount.LOWEST,
-        lowerTick,
-        upperTick,
-    ]);
-    const muniProxy = await deployContract({
-        name: 'DFX MUNI dfxCAD/CADC',
-        deployer: wallet,
-        factory: UpgradableProxyFactory,
-        args: [
-            muniLogicV1.address,
-            wallet.address,
-            calldata
-        ],
-        opts: {
-            // gasLimit: 16678090
-        }
-    });
+  // Deploy ASCUpgradableProxy with the encoded args and initialize MUNI pool
+  const calldata = MUNILogicFactory.interface.encodeFunctionData("initialize", [
+    DFX_GOV_MULTISIG, // owner
+    poolAddress,
+    FeeAmount.LOWEST,
+    lowerTick,
+    upperTick,
+  ]);
+  const muniProxy = await deployContract({
+    name: "DFX MUNI dfxCAD/CADC",
+    deployer: wallet,
+    factory: UpgradableProxyFactory,
+    args: [muniLogicV1.address, wallet.address, calldata],
+    opts: {
+      // gasLimit: 16678090
+    },
+  });
 
-    // Output to file
-    const output = {
-        muniProxy: muniProxy.address,
-        calldata: {
-            muni: ethers.utils.defaultAbiCoder.encode(["address", "address", "uint", "int", "int"], [
-                wallet.address,
-                poolAddress,
-                FeeAmount.LOWEST,
-                lowerTick,
-                upperTick,
-            ]),
-        }
-    };
+  // Output to file
+  const output = {
+    muniProxy: muniProxy.address,
+    calldata: {
+      muni: ethers.utils.defaultAbiCoder.encode(
+        ["address", "address", "uint", "int", "int"],
+        [wallet.address, poolAddress, FeeAmount.LOWEST, lowerTick, upperTick]
+      ),
+    },
+  };
 
-    const __filename = fileURLToPath(import.meta.url);
-    const __dirname = path.dirname(__filename);
-    const outputPath = path.join(__dirname, new Date().getTime().toString() + `_uniswapv3_dfxcad-cadc.json`);
-    fs.writeFileSync(outputPath, JSON.stringify(output, null, 4));
-}
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = path.dirname(__filename);
+  const outputPath = path.join(
+    __dirname,
+    new Date().getTime().toString() + `_uniswapv3_dfxcad-cadc.json`
+  );
+  fs.writeFileSync(outputPath, JSON.stringify(output, null, 4));
+};
 
 // We recommend this pattern to be able to use async/await everywhere
 // and properly handle errors.
 main()
-    .then(() => process.exit(0))
-    .catch(error => {
-        console.error(error);
-        process.exit(1);
-    });
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
