acee561fc754dca04a5284ae294723ac9a932ea1
K Fitzsimmons <kafitz22@gmail.com>
2022-06-17 11:18:58 -0400

    Fix async function call

--- scripts/05-initialize-muni-logic.js
+++ scripts/05-initialize-muni-logic.js
@@ -9,13 +9,13 @@ import MUNILogicV1Artifact from "../out/MUNILogicV1.sol/MUNILogicV1.json";
 const UNISWAP_V3_POOL = "0xFca463b342891CdfDc77180A694582BF1fc6C954";
 
 // MUNI Logic Contract (base, non-proxied)
-const MUNI_LOGIC = "0xA2bF8A967a54339D745160b08fe2e9813bD62471";
+const MUNI_LOGIC = "0x9715958087842E538e79577d1624D4F736687fdc";
 
 // Access controller
 const DFX_GOV_MULTISIG = "0x27E843260c71443b4CC8cB6bF226C3f77b9695AF";
 
 const main = async () => {
-    const muniLogicV1 = new ethers.Contract(MUNI_LOGIC, MUNILogicV1Artifact, wallet);
+    const muniLogicV1 = new ethers.Contract(MUNI_LOGIC, MUNILogicV1Artifact.abi, wallet);
     const dfxCadCadcPool = new ethers.Contract(UNISWAP_V3_POOL, IUniswapV3Artifact.abi, wallet);
  
     const tickSpacing = await dfxCadCadcPool.tickSpacing();
@@ -26,13 +26,14 @@ const main = async () => {
     const lowerTick = lowerTickAmount - (lowerTickAmount % tickSpacing);
     const upperTick = upperTickAmount - (upperTickAmount % tickSpacing);
 
-    muniLogicV1.initialize(
+    const tx = await muniLogicV1.initialize(
         DFX_GOV_MULTISIG, // owner
         UNISWAP_V3_POOL,
         FeeAmount.LOWEST,
         lowerTick,
         upperTick,        
     )
+    tx.wait();
 }
 
 // We recommend this pattern to be able to use async/await everywhere
